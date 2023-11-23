800651d3d40def5b1c59379c606a65a396b58d26
Asharib Ali <asharibarain4@gmail.com>
2023-11-23 22:30:27 +0500

    update

---
+++ day-55/script.js
@@ -0,0 +1,41 @@
+console.log("first");
+
+let a = 5;
+let b = 6;
+let c = "Harry";
+let _a = "asharib";
+let $a = "asharib";
+
+// console.log(a + b + 9);
+// console.log(typeof a, typeof b, typeof c);
+
+{
+  //   let a = 66;
+  console.log(a);
+}
+console.log(a);
+
+// const a1 = 6;
+// a1 = a1 = 20;
+
+// Not Allowed because A1 is contant
+
+let x = "Asharib Ali";
+let y = 22;
+let z = 3.55;
+let k = true;
+let l = null;
+let m = undefined;
+
+console.log(typeof x, typeof y, typeof z, typeof k, typeof l, typeof m);
+
+let o = {
+  name: "Asharib",
+  "Job code": 5600,
+  is_handsome: true,
+};
+console.log(o);
+o.salary = "1000000";
+console.log(o);
+o.salary = "5000000";
+console.log(o);
---
+++ day-56/index.js
@@ -0,0 +1,17 @@
+let age = 19;
+// let grace = 3;
+
+// age %= grace;
+
+// console.log(age);
+// console.log(age - grace);
+// console.log(age * grace);
+// console.log(age / grace);
+// console.log(age ** grace);
+// console.log(age % grace);
+
+if (age != 18) {
+  console.log("You can drive");
+} else {
+  console.log("You cannot drive");
+}
