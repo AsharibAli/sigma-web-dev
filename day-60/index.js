6af22a7d1abe65e359fdb19403e53b377dbd14ae
Asharib Ali <asharibarain4@gmail.com>
2023-11-25 17:34:48 +0500

    update: add new day

---
+++ day-60/index.js
@@ -0,0 +1,29 @@
+console.log("This is JavaScript Strings Tutorial");
+
+let a = "Asharib";
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+console.log(a[5]);
+console.log(a[6]);
+
+console.log(a.length);
+let real_name = "Asharib";
+let friend = "Aman";
+console.log(`His name is ${real_name} his friends name is ${friend}`);
+
+let b = "Shivam SH";
+// console.log(b.toUpperCase());
+// console.log(b.toLowerCase());
+// console.log(b.length);
+// console.log(b.slice(2, 6));
+// console.log(b.slice(3));
+
+let thename = "Asharib Bhai";
+let newName = thename.replace("Asharib", "Bhao");
+console.log(newName);
+console.log(b.concat(a, " Aishways", " crypto", +18));
+console.log(b.trim());
+console.log(b);
