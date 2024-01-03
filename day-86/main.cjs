7d97fff3ff8c71001a97d048d73c41a66f1373cf
Asharib Ali <asharibarain4@gmail.com>
2024-01-03 18:21:24 +0500

    Add new files and update existing files

---
+++ day-86/main.cjs
@@ -0,0 +1,9 @@
+// import { a, b, c, d, e } from "./day-86/mymodules";
+// console.log(a, b, c, d, e);
+
+// import obj from "./day-86/mymodules.js";
+// console.log(obj);
+
+const a = require("./mymodule2.js");
+
+console.log(a, __dirname, __filename);
---
+++ day-86/mymodules.js
@@ -0,0 +1,10 @@
+export const a = 1;
+export const b = 2;
+export const c = 3;
+export const d = 4;
+export const e = 5;
+
+export default obj = {
+  x: 1,
+  y: 2,
+};
