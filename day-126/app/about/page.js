2c4c33f3e911d40e41307438c3be4fface50ea71
Asharib Ali <asharibarain4@gmail.com>
2024-03-03 10:52:14 +0500

    update

---
+++ day-126/app/about/page.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1>About Page</h1>
+    </div>
+  );
+};
---
+++ day-126/middleware.js
@@ -0,0 +1,23 @@
+// import { NextResponse } from "next/server";
+
+// // This function can be marked "async" if using "await" inside
+// export function middleware(request) {
+//   return NextResponse.redirect(new URL("/", request.url));
+// }
+
+// // See "Matching Paths" below to learn more
+// export const config = {
+//   matcher: "/about/:path*",
+// };
+
+import { NextResponse } from "next/server";
+
+export function middleware(request) {
+  if (request.nextUrl.pathname.startsWith("/about")) {
+    return NextResponse.rewrite(new URL("/", request.url));
+  }
+
+  if (request.nextUrl.pathname.startsWith("/dashboard")) {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+}
